eecd82dae3648acb9ef27e73162c52034436dd9a
Learn <learn@learn14.local>
2017-05-22 16:44:43 -0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import FormInput from './components/FormInput'
+import RegistrationStore from './stores/RegistrationStore'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      registration: {
-        firstName:'',
-        lastName:'',
-        email:'',
-        password:''
-      }
+      registration: RegistrationStore.getFields(),
+      errors: {}
     }
   }
 
@@ -22,122 +20,118 @@ class App extends Component {
       registration: registration
     })
   }
-
+  validate(){
+    RegistrationStore.validate()
+    this.setState({errors: RegistrationStore.getErrors()})
+  }
   handleSubmit(event){
     event.preventDefault()
+    this.validate()
     console.log(this.state.registration)
+  }
+
+  isValid(){
+    return Object.keys(this.state.errors).length === 0
   }
 
   render() {
     return (
       <div>
-        <Header />
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xs-6 col-xs-offset-3'>
-              <div className='panel panel-default'>
-                <div className='panel-body'>
-                  <h3>Registration</h3>
-                  <form onSubmit={this.handleSubmit.bind(this)}>
-                    <div className='row'>
-                      <div className='col-xs-12'>
-                        <div className='form-group'> 
-                          <label 
-                            htmlFor='firstName'
-                            className='control-label'
-                          >
-                            First Name
-                          </label>
-                          <input
-                            name='firstName'
-                            value={this.state.registration.firstName}
-                            onChange={this.handleChange.bind(this)}
-                            className='form-control'
-                          />
-                        </div>
-                      </div>
-                    </div>
+      <Header />
+      <div className='container'>
+      <div className='row'>
+      <div className='col-xs-6 col-xs-offset-3'>
+      <div className='panel panel-default'>
+      <div className='panel-body'>
+      { !this.isValid() &&
+      <div className='alert alert-danger'>
+        Verify fields below
+      </div>
+      }
+      <h3>Registration</h3>
 
-                    <div className='row'>
-                      <div className='col-xs-12'>
-                        <div className='form-group'>
-                          <label 
-                            htmlFor='lastName'
-                            className='control-label'
-                          >
-                            Last Name
-                          </label>
-                          <input
-                            name='lastName'
-                            value={this.state.registration.lastName}
-                            onChange={this.handleChange.bind(this)}
-                            className='form-control'
-                          />
-                        </div>
-                      </div>
-                    </div>
+      {/* ENTIRE FORM */}
+      <form onSubmit={this.handleSubmit.bind(this)}>
 
-                    <div className='row'>
-                      <div className='col-xs-12'>
-                        <div className='form-group'>
-                          <label 
-                            htmlFor='email'
-                            className='control-label'
-                          >
-                            Email
-                          </label>
-                          <input
-                            name='email'
-                            value={this.state.registration.email}
-                            onChange={this.handleChange.bind(this)}
-                            className='form-control'
-                          />
-                        </div>
-                      </div>
-                    </div>
+      {/* FIRST NAME */}
+      <div className='row'>
+      <div className='col-xs-12'>
+      <FormInput
+      name='firstName'
+      label='First Name'
+      value={this.state.registration.firstName}
+      onChange={this.handleChange.bind(this)}
+      errors={this.state.errors.firstName}
+      />
+      </div>
+      </div>
 
-                    <div className='row'>
-                      <div className='col-xs-12'>
-                        <div className='form-group'>
-                          <label 
-                            htmlFor='password'
-                            className='control-label'
-                          >
-                            Password
-                          </label>
-                          <input
-                            type='password'
-                            name='password'
-                            value={this.state.registration.password}
-                            onChange={this.handleChange.bind(this)}
-                            className='form-control'
-                          />
-                        </div>
-                      </div>
-                    </div>
+      {/* LAST NAME */}
+      <div className='row'>
+      <div className='col-xs-12'>
+      <FormInput
+      name='lastName'
+      label='Last Name'
+      value={this.state.registration.lastName}
+      onChange={this.handleChange.bind(this)}
+      errors={this.state.errors.lastName}
+      />
+      </div>
+      </div>
 
-                    <div className='row'>
-                      <div className='col-xs-12'>
-                        <input className='btn btn-primary' type='submit' value='Submit' />
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr />
-        <div className='row'>
-          <div className="col-xs-4 col-xs-offset-4">
-            <ul className='list-group'>
-              <li className='list-group-item'>First Name: {this.state.registration.firstName}</li>
-              <li className='list-group-item'>Last Name: {this.state.registration.lastName}</li>
-              <li className='list-group-item'>Email: {this.state.registration.email}</li>
-              <li className='list-group-item'>Password: {this.state.registration.password}</li>
-            </ul>
-          </div>
-        </div>
+      {/* EMAIL */}
+      <div className='row'>
+      <div className='col-xs-12'>
+      <FormInput
+      name='email'
+      label='Email'
+      value={this.state.registration.email}
+      onChange={this.handleChange.bind(this)}
+      errors={this.state.errors.email}
+      />
+      </div>
+      </div>
+
+      {/* PASSWORD */}
+      <div className='row'>
+      <div className='col-xs-12'>
+      <FormInput
+      type='password'
+      name='password'
+      label='Password'
+      value={this.state.registration.password}
+      onChange={this.handleChange.bind(this)}
+      errors={this.state.errors.password}
+      />
+      </div>
+      </div>
+
+      {/* SUBMIT */}
+      <div className='row'>
+      <div className='col-xs-12'>
+      <input className='btn btn-primary' type='submit' value='Submit' />
+      </div>
+      </div>
+
+      </form>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      <hr />
+
+      {/* OUTPUT */}
+      <div className='row'>
+      <div className="col-xs-4 col-xs-offset-4">
+      <ul className='list-group'>
+      <li className='list-group-item'>First Name: {this.state.registration.firstName}</li>
+      <li className='list-group-item'>Last Name: {this.state.registration.lastName}</li>
+      <li className='list-group-item'>Email: {this.state.registration.email}</li>
+      <li className='list-group-item'>Password: {this.state.registration.password}</li>
+      </ul>
+      </div>
+      </div>
       </div>
     );
   }
